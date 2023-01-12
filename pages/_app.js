9c021113a1934a634b63c71f2c91f1ea91b8daa9
Marti Serra <martiserramolina99@gmail.com>
2023-01-12 18:50:39 +0100

    Setup metadata in App

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import "styles/globals.scss";
 
+import Head from "next/head";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>
+          Canvas User Interface - The JavaScript Library To Build Canvas User
+          Interfaces
+        </title>
+        <link rel="icon" href="/favicon/favicon.png" />
+        <link rel="apple-touch-icon" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta charSet="utf-8" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
