1a71dfb312e78ecdfbbdeeb86346180c46e7a710
Marti Serra <martiserramolina99@gmail.com>
2023-01-13 13:44:07 +0100

    Add docs right component

--- src/components/docs/components/right/right.js
+++ src/components/docs/components/right/right.js
@@ -1,5 +1,57 @@
 import styles from "./right.module.scss";
 
+import Link from "next/link";
+
 export default function Right() {
-  return <div className={styles.right}>B</div>;
+  return (
+    <div className={styles.right}>
+      <h4 className={styles.title}>On This Page</h4>
+      <ul className={styles.sections}>
+        <li className={styles.section}>
+          <Link href="#" className={styles.link}>
+            Overview
+          </Link>
+          <ul className={styles.subsections}>
+            <li className={styles.subsection}>
+              <Link href="#" className={styles.link}>
+                Overview
+              </Link>
+            </li>
+            <li className={styles.subsction}>
+              <Link href="#" className={styles.link}>
+                Installation
+              </Link>
+            </li>
+            <li className={styles.subsection}>
+              <Link href="#" className={styles.link}>
+                Quick Start
+              </Link>
+            </li>
+          </ul>
+        </li>
+        <li className={styles.section}>
+          <Link href="#" className={styles.link}>
+            Overview
+          </Link>
+          <ul className={styles.subsections}>
+            <li className={styles.subsection}>
+              <Link href="#" className={styles.link}>
+                Overview
+              </Link>
+            </li>
+            <li className={styles.subsction}>
+              <Link href="#" className={styles.link}>
+                Installation
+              </Link>
+            </li>
+            <li className={styles.subsection}>
+              <Link href="#" className={styles.link}>
+                Quick Start
+              </Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
 }
