56b6d8db7d6e98023761dc770f94f2528758048c
Marti Serra <martiserramolina99@gmail.com>
2023-01-13 13:19:42 +0100

    Setup docs left component

--- src/components/docs/components/left/left.js
+++ src/components/docs/components/left/left.js
@@ -1,5 +1,72 @@
 import styles from "./left.module.scss";
 
+import Link from "next/link";
+
 export default function Left() {
-  return <div className={styles.left}>A</div>;
+  return (
+    <nav className={styles.left}>
+      <ul className={styles.sections}>
+        <li className={styles.section}>
+          <h3 className={styles.heading}>Getting Started</h3>
+          <ul className={styles.pages}>
+            <li>
+              <Link href="#" className={styles.link}>
+                Overview
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className={styles.link}>
+                Installation
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className={styles.link}>
+                Quick Start
+              </Link>
+            </li>
+          </ul>
+        </li>
+        <li className={styles.section}>
+          <h3 className={styles.heading}>Getting Started</h3>
+          <ul className={styles.pages}>
+            <li>
+              <Link href="#" className={styles.link}>
+                Overview
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className={styles.link}>
+                Installation
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className={styles.link}>
+                Quick Start
+              </Link>
+            </li>
+          </ul>
+        </li>
+        <li className={styles.section}>
+          <h3 className={styles.heading}>Getting Started</h3>
+          <ul className={styles.pages}>
+            <li>
+              <Link href="#" className={styles.link}>
+                Overview
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className={styles.link}>
+                Installation
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className={styles.link}>
+                Quick Start
+              </Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  );
 }
