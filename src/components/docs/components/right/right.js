a29adb896b595dd1913d8402fa08d9570eb4baaf
Marti Serra <martiserramolina99@gmail.com>
2023-01-15 15:08:26 +0100

    Set position sticky to docs right

--- src/components/docs/components/right/right.js
+++ src/components/docs/components/right/right.js
@@ -5,29 +5,31 @@ import Link from "next/link";
 export default function Right({ slug, right }) {
   return (
     <div className={styles.right}>
-      <h4 className={styles.title}>On This Page</h4>
-      <ul className={styles.sections}>
-        {right.length === 0 ? (
-          <span className={styles.empty}>There are no sections.</span>
-        ) : (
-          right.map((section) => (
-            <li key={section.slug}>
-              <Link href="#" className={styles.link}>
-                {section.name}
-              </Link>
-              <ul className={styles.subsections}>
-                {section.subsections.map((subsection) => (
-                  <li key={subsection.slug}>
-                    <Link href="#" className={styles.link}>
-                      {subsection.name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))
-        )}
-      </ul>
+      <div className={styles.content}>
+        <h4 className={styles.title}>On This Page</h4>
+        <ul className={styles.sections}>
+          {right.length === 0 ? (
+            <span className={styles.empty}>There are no sections.</span>
+          ) : (
+            right.map((section) => (
+              <li key={section.slug}>
+                <Link href="#" className={styles.link}>
+                  {section.name}
+                </Link>
+                <ul className={styles.subsections}>
+                  {section.subsections.map((subsection) => (
+                    <li key={subsection.slug}>
+                      <Link href="#" className={styles.link}>
+                        {subsection.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
