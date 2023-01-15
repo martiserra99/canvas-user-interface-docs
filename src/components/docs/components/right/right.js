83e8eddf772c7c485fb5e9cfa6077d937c6af6af
Marti Serra <martiserramolina99@gmail.com>
2023-01-15 14:13:24 +0100

    Add message empty in right docs

--- src/components/docs/components/right/right.js
+++ src/components/docs/components/right/right.js
@@ -7,22 +7,26 @@ export default function Right({ slug, right }) {
     <div className={styles.right}>
       <h4 className={styles.title}>On This Page</h4>
       <ul className={styles.sections}>
-        {right.map((section) => (
-          <li key={section.slug}>
-            <Link href="#" className={styles.link}>
-              {section.name}
-            </Link>
-            <ul className={styles.subsections}>
-              {section.subsections.map((subsection) => (
-                <li key={subsection.slug}>
-                  <Link href="#" className={styles.link}>
-                    {subsection.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
+        {right.length === 0 ? (
+          <span className={styles.empty}>There are no sections.</span>
+        ) : (
+          right.map((section) => (
+            <li key={section.slug}>
+              <Link href="#" className={styles.link}>
+                {section.name}
+              </Link>
+              <ul className={styles.subsections}>
+                {section.subsections.map((subsection) => (
+                  <li key={subsection.slug}>
+                    <Link href="#" className={styles.link}>
+                      {subsection.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))
+        )}
       </ul>
     </div>
   );
