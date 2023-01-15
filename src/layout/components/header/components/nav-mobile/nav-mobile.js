3c170e3d46d66d113ee512049389882e4e32cc1d
Marti Serra <martiserramolina99@gmail.com>
2023-01-15 09:22:32 +0100

    Setup links in nav and nav-mobile

--- src/layout/components/header/components/nav-mobile/nav-mobile.js
+++ src/layout/components/header/components/nav-mobile/nav-mobile.js
@@ -1,6 +1,7 @@
 import styles from "./nav-mobile.module.scss";
 
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useRef } from "react";
 import { Transition } from "react-transition-group";
 
@@ -9,6 +10,7 @@ import IcLinkedin from "../ic-linkedin";
 import IcWeb from "../ic-web";
 
 export default function NavMobile({ open }) {
+  const router = useRouter();
   const nodeRef = useRef();
   return (
     <Transition nodeRef={nodeRef} in={open} timeout={200}>
@@ -16,30 +18,55 @@ export default function NavMobile({ open }) {
         <div ref={nodeRef} className={styles.nav + " " + styles[state]}>
           <div className={styles.content + " " + styles[state]}>
             <div className={styles.links}>
-              <Link href="/" className={styles.link + " " + styles.selected}>
+              <Link
+                href="/how-to-use/getting-started/overview"
+                className={linkClassName(router, "/how-to-use")}
+              >
                 How To Use
               </Link>
-              <Link href="/" className={styles.link}>
+              <Link
+                href="/elements/views/area"
+                className={linkClassName(router, "/elements")}
+              >
                 Elements
               </Link>
-              <Link href="/" className={styles.link}>
+              <Link
+                href="/examples"
+                className={linkClassName(router, "/examples")}
+              >
                 Examples
               </Link>
             </div>
             <div className={styles.icons}>
-              <a href="#" className={styles.icon}>
+              <a
+                href="https://github.com/martiserra99"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.icon}
+              >
                 <IcGithub fill="inherit" />
               </a>
-              <a href="#" className={styles.icon}>
+              <a
+                href="https://www.linkedin.com/in/mart%C3%AD-serra-molina-3ababa23a/"
+                target="_blank"
+                rel="noreferrer"
+                className={styles.icon}
+              >
                 <IcLinkedin fill="inherit" />
               </a>
-              <a href="#" className={styles.icon}>
+              {/* <a href="#" className={styles.icon}>
                 <IcWeb fill="inherit" />
-              </a>
+              </a> */}
             </div>
           </div>
         </div>
       )}
     </Transition>
   );
+}
+
+function linkClassName(router, path) {
+  let className = styles.link;
+  if (router.pathname.startsWith(path)) className += ` ${styles.selected}`;
+  return className;
 }
--- src/layout/components/header/components/nav/nav.js
+++ src/layout/components/header/components/nav/nav.js
@@ -1,36 +1,60 @@
 import styles from "./nav.module.scss";
 
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 import IcGithub from "src/layout/components/header/components/ic-github";
 import IcLinkedin from "src/layout/components/header/components/ic-linkedin";
 import IcWeb from "src/layout/components/header/components/ic-web";
 
 export default function Nav() {
+  const router = useRouter();
   return (
     <nav className={styles.nav}>
       <div className={styles.links}>
-        <Link href="/" className={styles.link + " " + styles.selected}>
+        <Link
+          href="/how-to-use/getting-started/overview"
+          className={linkClassName(router, "/how-to-use")}
+        >
           How To Use
         </Link>
-        <Link href="/" className={styles.link}>
+        <Link
+          href="/elements/views/area"
+          className={linkClassName(router, "/elements")}
+        >
           Elements
         </Link>
-        <Link href="/" className={styles.link}>
+        <Link href="/examples" className={linkClassName(router, "/examples")}>
           Examples
         </Link>
       </div>
       <div className={styles.icons}>
-        <a href="#" className={styles.icon}>
+        <a
+          href="https://github.com/martiserra99"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.icon}
+        >
           <IcGithub fill="inherit" />
         </a>
-        <a href="#" className={styles.icon}>
+        <a
+          href="https://www.linkedin.com/in/mart%C3%AD-serra-molina-3ababa23a/"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.icon}
+        >
           <IcLinkedin fill="inherit" />
         </a>
-        <a href="#" className={styles.icon}>
+        {/* <a href="#" className={styles.icon}>
           <IcWeb fill="inherit" />
-        </a>
+        </a> */}
       </div>
     </nav>
   );
+}
+
+function linkClassName(router, path) {
+  let className = styles.link;
+  if (router.pathname.startsWith(path)) className += ` ${styles.selected}`;
+  return className;
 }
