b0d87ea2cb8ec36a81f5ab756a4431c19a0f8508
Marti Serra <martiserramolina99@gmail.com>
2023-01-15 19:41:48 +0100

    Remove container class in docs

--- src/components/docs/docs.js
+++ src/components/docs/docs.js
@@ -1,14 +1,15 @@
-import Container from "./components/container/container";
+import styles from "./docs.module.scss";
+
 import Content from "./components/content/content";
 import Left from "./components/left/left";
 import Right from "./components/right/right";
 
 export default function Docs({ slug, left, right, content }) {
   return (
-    <Container>
+    <div className={styles.docs}>
       <Left slug={slug} left={left} />
       <Content content={content} />
       <Right slug={slug} right={right} />
-    </Container>
+    </div>
   );
 }
