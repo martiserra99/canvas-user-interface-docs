023f324aef776bc0d74190734633cf65383cca20
Marti Serra <martiserramolina99@gmail.com>
2023-01-17 04:15:45 +0100

    Improve home text

--- src/pages/home/components/features.js
+++ src/pages/home/components/features.js
@@ -1,4 +1,4 @@
-import styles from "./features.module.scss";
+import styles from "./features.module.scss"
 
 export default function Features() {
   return (
@@ -6,24 +6,24 @@ export default function Features() {
       <div className={styles.feature}>
         <h2>Approachable</h2>
         <p>
-          A truly simple and yet powerful API to build user interfaces using UI
+          A truly simple and yet powerful API to build user interfaces using
           elements.
         </p>
       </div>
       <div className={styles.feature}>
         <h2>Extensible</h2>
         <p>
-          Completely extensible providing you ways to build your own types of UI
+          Completely extensible, providing you ways to build your own types of
           elements.
         </p>
       </div>
       <div className={styles.feature}>
         <h2>Versatile</h2>
         <p>
-          Can be used for different purposes giving you a way to do anything you
-          want.
+          It can be used for different purposes, giving you a way to do anything
+          you want.
         </p>
       </div>
     </section>
-  );
+  )
 }
